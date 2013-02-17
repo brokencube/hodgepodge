7a8eecb84be76d7ace2e809867a40ee1647f6813
Nick Barham <nick.barham@midnighttonic.com>
2013-02-17 16:42:52 +0000

    Rewrote Router.php to use new URL schema. All processing of URL is now done within a single regex rather than using multiple explodes and array manipulation.

--- Core/Router.php
+++ Core/Router.php
@@ -3,113 +3,93 @@ namespace LibNik\Core;
 
 class Router
 {
-    protected static $page;
-    protected static $filename;
-    protected static $args;
-    protected static $path;
+    protected static $path;                // Full URL path as passed to router
+    protected static $filename;            // Local path to php file as calculated from URL
+    protected static $args;                // Array of secondary arguments passed as directories within the url
+    protected static $default = 'home';    // Path to default to if no path detected
+    protected static $lookups = array(     // List of locations of files for particular file extensions (.html, .ajax etc)
+        'html' => 'webroot'
+    );
     
-    // Based on the manipulation of the URL by mod_rewrite, decide what php file in webroot to include.
-    // Will look for mobile.<name>.php first if in mobile browser mode.
+    // Based on the manipulation of the URL by mod_rewrite, decide what php file in to load.
     public static function route($routepath = null)
     {
-        global $config;
+        static::$path = $routepath ?: $_REQUEST['routepath'];
         
-        if (!$routepath) $routepath = $_REQUEST['routepath'];
+        // Regex the path to extract information
+        /* Examples:
+         * example.com/home                           -> (home) () ()
+         * example.com/home/search/everything.ajax    -> (home) (search/everything) (ajax)
+         * example.com/home/search                    -> (home) (search) ()
+         * example.com/search.ajax                    -> (search) () (ajax)
+         * example.com                                -> (home) () ()        // 'home' taken from static::$default!
+         */
+        preg_match(
+            '/
+                ^\/             # Anchor to start of path
+                ([^.\/]+)       # Grab the first part of the path (to be the filename), until either a dot or forwardslash is found
+                \/?(.*?)        # Grab the middle of the path (directory structure after first level)
+                (?:\.([^\/]+))? # Optionally, grab an "extension" e.g.   ".ajax" at the end of the path
+                $               # Anchor to end of path
+            /x',
+            static::$path,
+            $matches
+        );
         
-        // Get an array of path parts
-        static::$path = $parts = array_filter(explode('/', $routepath));
+        // Split up matches from above into sensibly named varibles;
+        if ($matches) list($full, $file, $args, $extension) = $matches;
+        if (!$file) $file = static::$default;
         
-        // Look for special parts of the url
-        while(true) {
-            $part = array_shift($parts);
-            
-            // No part? Home page!
-            if (!$part) {
-                $part = 'home';
-                break;
-            }
-            
-            // If the part is a special string (e.g. /ajax/)
-            if (!$special and $config['router']['files'] and array_search( $part, $config['router']['files']() ) !== false) {
-                $special = '.' . $part;
-                continue;
-            }
-            
-            // If the part is a special string (e.g. /dashboard/)
-            if (!$dir and $config['router']['dirs'] and array_search( $part, $config['router']['dirs']() ) !== false) {
-                $dir = $part . '/';
-                continue;
-            }
-            
-            break;
-        }
-        
-        // Mobile specific file?
-        if (Env::get()->mobileBrowser() && file_exists($dir . 'mobile.' . $part . $special . '.php')) {
-            $mobile = 'mobile.';
+        // Lookup specific extensions for alternate directories
+        if ($extension) {
+            $dir = static::$lookups[$extension];
+        } elseif (Env::get()->mobileBrowser()) {
+            $dir = static::$lookups['mobile'];
+        } else {
+            $dir = static::$lookups['html'];
         }
         
         // Assemble the desired filename
-        static::$filename = $dir . $mobile . $part . $special;
-        static::$page = $dir.$part;
-        
-        // Left over parts are argument list
-        static::$args = $parts;
+        static::$filename = $dir . '/' . $file . '.php';
+        static::$args = array_filter(explode('/', $args));
         
         return static::$filename;
     }
     
     public static function args()
     {
-        if (!isset(self::$args)) self::route();
-        return self::$args;
+        if (!isset(static::$path)) static::route();
+        return static::$args;
     }
 
     public static function path()
     {
-        if (!isset(self::$path)) self::route();
-        return self::$path;
-    }
-
-    public static function page()
-    {
-        if (!isset(self::$page)) self::route();
-        return self::$page;
-    }
-
-    public static function make($obj)
-    {
-        return '/';
+        if (!isset(static::$path)) static::route();
+        return static::$path;
     }
     
     // Auto-repairs urls - if the current url doesn't match the url for the object in question, then redirect to the objects URL.
     public static function autofix($url)
     {
-        if (is_object($url)) {
-            $url = static::make($url);
-        }
-        
         // Strip the hash from the end of the url
         if(strpos($url, '#') !== false) $url = substr($url, 0, strpos($url, '#'));
         if(strpos($url, '?') !== false) $url = substr($url, 0, strpos($url, '?'));
         
         if ($url && $url != $_SERVER['SCRIPT_URL']) {
-            redirect($url);
+            static::redirect($url);
         }
     }
     
     public function secure($be_secure = true)
     {
-        if (is_null($be_secure) or !Env::live_mode()) return;
-        
         // Force https
         if ($be_secure and $_SERVER['HTTPS'] != 'on') {
-            redirect('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+            static::redirect('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
         }
         
         // Force http
         if (!$be_secure and $_SERVER['HTTPS'] == 'on') {
-            redirect('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+            static::redirect('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
         }        
     }
     
@@ -126,5 +106,11 @@ class Router
         $string = preg_replace('/-+/', '-', $string);
         
         return $string;
+    }
+    
+    public static function redirect($url)
+    {
+        header("Location: $url");
+        exit;
     }
 }
