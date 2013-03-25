cb58b262ca731d834894b32d8e66cdf4e0573e76
Nick Barham <nick.barham@midnighttonic.com>
2013-03-25 16:44:49 +0000

    Removing internal $variable, instead exposing $source to allow validation to be staged.

--- lib/LibNik/Core/Validation.php
+++ lib/LibNik/Core/Validation.php
@@ -5,7 +5,6 @@ use LibNik\Exception;
 
 class Validation
 {
-    protected $variables = array();
     protected $name = '';
     protected $errors = array();
     protected $source = array();
@@ -28,19 +27,18 @@ class Validation
     
     public function __get($var)
     {
-        return $this->variables[$var];
+        return $this->source[$var];
     }
     
     public function __invoke($name)
     {
-        $this->variables[$name] = $this->source[$name];
         $this->current = $name;
         return $this;
     }
     
     public function required($message = "This field is required")
     {
-        if (!$this->variables[$this->current]) {
+        if (!$this->source[$this->current]) {
             $this->errors[$this->current] = $message;
         }
         return $this;
@@ -48,7 +46,7 @@ class Validation
     
     public function min($min = 3, $message = "This field must be at least 3 characters long")
     {
-        if (strlen($this->variables[$this->current]) < $min) {
+        if (strlen($this->source[$this->current]) < $min) {
             $this->errors[$this->current] = $message;
         }        
         return $this;
@@ -56,7 +54,7 @@ class Validation
     
     public function max($max = 3, $message = "This field must not be longer than 3 characters")
     {
-        if (strlen($this->variables[$this->current]) > $max) {
+        if (strlen($this->source[$this->current]) > $max) {
             $this->errors[$this->current] = $message;
         }        
         return $this;
@@ -64,7 +62,7 @@ class Validation
     
     public function email($message = "This is not a valid email address")
     {
-        if (filter_var($this->variables[$this->current], FILTER_VALIDATE_EMAIL) === FALSE) {
+        if (filter_var($this->source[$this->current], FILTER_VALIDATE_EMAIL) === FALSE) {
             $this->errors[$this->current] = $message;
         }        
         return $this;
@@ -72,7 +70,7 @@ class Validation
     
     public function matches($var = 'password', $message = "Passwords don't match")
     {
-        if ($this->variables[$this->current] != $this->variables[$var]) {
+        if ($this->source[$this->current] != $this->source[$var]) {
             $this->errors[$this->current] = $message;
         }                
         return $this;
