29e5636b8d706f6d05dd99fc77d2c48114060ba3
Nick Barham <nick.barham@midnighttonic.com>
2013-07-05 16:23:22 +0100

    Minor rework of how Orm/Time sets Timezones

--- lib/LibNik/Orm/Time.php
+++ lib/LibNik/Orm/Time.php
@@ -4,21 +4,26 @@ namespace LibNik\Orm;
 class Time extends \DateTime
 {
     const MYSQL_DATE = 'Y-m-d H:i:s';
-	public static $display_format = 'D jS M Y H:i:s T';
-	public static $display_timezone = 'UTC'; // Default to UTC
+	public static $format = 'D jS M Y H:i:s T';
+	public static $timezone = 'UTC'; // Default to UTC
 	
+    public function __construct($time = 'now', DateTimeZone $timezone)
+    {
+        if (!$timezone) $timezone = DateTimeZone(self::$timezone);
+        parent::__construct($time, $timezone);
+    }
+    
 	public function __toString()
 	{
-		$this->setTimezone(new \DateTimeZone(self::$display_timezone));
-		$datetime = $this->format(self::$display_format);	
-		$this->setTimezone(new \DateTimeZone('UTC'));
-		return $datetime;
+		return $this->format(self::$format);	
 	}
 	
 	public function mysql()
 	{
-		$this->setTimezone(new \DateTimeZone('UTC')); // Just to make doublely sure!
+		$timezone = $this->getTimezone();
+        $this->setTimezone(new \DateTimeZone('UTC')); // Just to make doubly sure!
 		$datetime = $this->format(self::MYSQL_DATE);
+        $this->setTimezone($timezone);
 		return $datetime;
 	}
 }
