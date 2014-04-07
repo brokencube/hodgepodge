54c4b0a7e88a9c059593b79816068020eacbca01
Nick Barham <nick.barham@musicgateway.net>
2014-04-07 15:17:49 +0100

    Adding custom callback function to Collection->filter()

--- lib/HodgePodge/Orm/Collection.php
+++ lib/HodgePodge/Orm/Collection.php
@@ -186,26 +186,44 @@ class Collection extends Common\Collection
         return new static($copy);
     }
 
-    public function filter($where_array)
+    public function filter($filter)
     {
         $copy = $this->container;
 
-        // Loop over items
-        foreach ($copy as $item_key => $item) {
-            // Loop over filters
-            foreach ($where_array as $property => $value_list) {
-                // Each filter can have several acceptable values -- force single item to array
-                if (!is_array($value_list)) $value_list = array($value_list);
-                // Check each value - if we find a matching value than skip to the next filter.
-                foreach ($value_list as $value) {
-                    if ($item->$property == $value) {
-                        continue 2;
+        if (is_array($filter))
+        {        
+            // Loop over items
+            foreach ($copy as $item_key => $item) {
+                // Loop over filters
+                foreach ($filter as $property => $value_list) {
+                    // Each filter can have several acceptable values -- force single item to array
+                    if (!is_array($value_list)) $value_list = array($value_list);
+                    // Check each value - if we find a matching value than skip to the next filter.
+                    foreach ($value_list as $value) {
+                        if ($item->$property == $value) {
+                            continue 2;
+                        }
                     }
+                    // Failed to break of loop, so the current value matches none of the
+                    // values for the current filter, therefore remove the item
+                    unset($copy[$item_key]);
                 }
-                // Failed to break of loop, so the current value matches none of the
-                // values for the current filter, therefore remove the item
-                unset($copy[$item_key]);
             }
+        }
+        elseif(is_callable($filter))
+        {
+            // Loop over items
+            foreach ($copy as $item_key => $item) {
+                // Use the closure/callback to filter the item
+                if (!$filter($item))
+                {
+                    unset($copy[$item_key]);
+                }
+            }            
+        }
+        else
+        {
+            throw new \InvalidArgumentException('Orm\Collection->filter() expects an array or callable');
         }
         
         $copy = array_values($copy);
