34dbcf4fc16552eeec962b993b39fbfd20a0c3ff
Nik Barham <nick.barham@musicgateway.net>
2014-07-14 08:33:53 +0000

    Adding "410 Gone" status

---
+++ lib/HodgePodge/Exception/HTTP/Gone.php
@@ -0,0 +1,12 @@
+<?php
+namespace HodgePodge\Exception\HTTP;
+
+class Gone extends \HodgePodge\Exception\HTTP
+{
+    protected $header = 'HTTP/1.1 410 Gone';
+    public function __construct($message = null, $data = null, \Exception $previous_exception = null)
+    {
+        parent::__construct(410, $message, $data, $previous_exception);
+    }
+}
+
