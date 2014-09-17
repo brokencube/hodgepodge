7c1cddf690c1b43e041aa5fa5fa90b3c88974af5
Nik Barham <nick.barham@musicgateway.net>
2014-09-17 09:56:57 +0000

    Much cleaner new Router

--- lib/HodgePodge/Core/Router.php
+++ lib/HodgePodge/Core/Router.php
@@ -3,57 +3,80 @@ namespace HodgePodge\Core;
 
 class Router
 {
-    protected static $path;                     // Full URL path as passed to router
-    protected static $filename;                 // Local path to php file as calculated from URL
-    protected static $parts = array();          // Array of secondary arguments passed as directories within the url
-    protected static $routed_parts = array();   // Parts of the path used to calculate local path
-    public static $default = 'home';            // Path to default to if no path detected
-    public static $route_prefix = 'webroot';    // List of locations of files for particular file extensions (.html, .ajax etc)
+    protected static $root;                         // Base_dir for this application
+    protected static $path;                         // URL path as passed to router
+                                                    // - e.g. https://www.example.com/[example/url]?query_string -> 'example/url'
+    protected static $filename;                     // Local path to php file as calculated from URL
+    protected static $parts = array();              // Array of secondary arguments passed as directories within the url
+    protected static $routed_parts = array();       // Parts of the path used to calculate local path
+    public static $default = 'home';                // Path to default to if no path detected
+    public static $extension = '.php';                // Path to default to if no path detected
+    public static $route_prefix = ['controllers'];  // List of controller locations (from /$root)
     
     // Based on the manipulation of the URL by mod_rewrite, decide what php file in to load.
-    public static function route($routepath = null)
+    public static function route($routepath = null, $root = null)
     {
+        // Calculate root (parameter -> global const -> guessed)
+        static::$root = $root ?: ROOT ?: dirname($_SERVER['DOCUMENT_ROOT']);
+        if (substr(static::$root, -1) != DIRECTORY_SEPARATOR) static::$root .= DIRECTORY_SEPARATOR;
+        
+        // Get path to route
         static::$path = $routepath ?: $_REQUEST['routepath'];
         static::$path = str_replace(chr(0), '', static::$path); // Protect against poison null byte attacks
         
         // Split the url on '/'
         static::$parts = array_filter(explode('/', static::$path));
+        if (!static::$parts[0]) static::$parts[0] = static::$default; // If first part is empty, replace with default
         
-        // Generate the first possible filename for this URL
-        return static::generateRoutedFilename();
-    }
-
-    protected static function generateRoutedFilename()
-    {
-        // Grab the next part of the URL and merge into the routed path
-        static::$routed_parts[] = array_shift(static::$parts) ?: static::$default;
-
-        static::$filename =
-            (static::$route_prefix ? static::$route_prefix . DIRECTORY_SEPARATOR : '')
-            . implode(DIRECTORY_SEPARATOR, static::$routed_parts)
-            . '.php';
+        // Keep pulling file path toeksn from static::$parts until we find a routable target, or run out of parts
+        do {
+            $continue_path_traversal = false;
+            
+            // Push next tokenised part of path onto include path
+            // (The remaining $parts are used as the controller arguments)
+            static::$routed_parts[] = array_shift(static::$parts);
+            
+            // Build filename
+            foreach(static::$route_prefix as $pre) {
+                $path = static::$root
+                    . $pre . DIRECTORY_SEPARATOR
+                    . implode(DIRECTORY_SEPARATOR, static::$routed_parts);
+                    
+                // If file exists, then include controller and return.
+                if(file_exists($path . static::$extension)) {
+                    static::$filename = $path . static::$extension;
+                    require_once(static::$filename);
+                    return true;
+                }
+                
+                if(is_dir($path)) {
+                    $continue_path_traversal = true;
+                }
+            }
+            
+            // Could not find a file to include
+            if (!$continue_path_traversal) {
+                // We did not detected a deeper matching directory we could traverse into. Bail out!
+                throw new HodgePodge\Exception\HTTP\NotFound();
+            }
+            
+        } while (count(static::$parts));
         
-        return static::$filename;
-    }
-
-    public static function reroute()
-    {
-        // Generate the next possible filename for this URL
-        return static::generateRoutedFilename();
+        // Could not find a page to route to - throw exception
+        throw new HodgePodge\Exception\HTTP\NotFound();
     }
     
     public static function args()
     {
-        if (!isset(static::$path)) static::route();
         return static::$parts;
     }
 
     public static function path()
     {
-        if (!isset(static::$path)) static::route();
         return static::$path;
     }
     
+    /* UTILITY FUNCTIONS */
     // Utility function to replace bad characters for urls
     public static function clean($url)
     {
@@ -69,10 +92,10 @@ class Router
         return $string;
     }
 
-    // Auto-repairs urls - if the current url doesn't match the url for the object in question, then redirect to the objects URL.
+    // Redirect to $url if current url is not $url
     public static function autofix($url)
     {
-        // Strip the hash from the end of the url
+        // Strip the hash + query string
         if(strpos($url, '#') !== false) $url = substr($url, 0, strpos($url, '#'));
         if(strpos($url, '?') !== false) $url = substr($url, 0, strpos($url, '?'));
         
@@ -81,7 +104,7 @@ class Router
         }
     }
     
-    public function secure($be_secure = true)
+    public static function secure($be_secure = true)
     {
         $env = Env::get();
         // Force https
@@ -97,6 +120,7 @@ class Router
         
     public static function redirect($url)
     {
+        $url = str_replace(chr(0), '', $url); // Protect against poison null byte attacks
         header("Location: $url");
         exit;
     }
