390951bf3563fb4012ad849982a2d7748194dca3
Nik Barham <nick.barham@musicgateway.net>
2015-08-21 15:18:53 +0000

    New URL clean for '£'

--- lib/HodgePodge/Core/Router.php
+++ lib/HodgePodge/Core/Router.php
@@ -81,8 +81,8 @@ class Router
     public static function clean($url)
     {
         $string = str_replace(
-            array('&', '%', '/', '\\', "'", '"', ':', '#', '@', '?', ',', '>', '<'),
-            array('and', '%25', ' '),
+            array('&', '%', '£', '/', '\\', "'", '"', ':', '#', '@', '?', ',', '>', '<'),
+            array('and', '%25', '%C2%A3', ' '),
             $url
         );
         
