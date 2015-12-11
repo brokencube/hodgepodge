c319d2af57c86ee34365e3ab809cb4c379a37ea2
Nik Barham <nick.barham@musicgateway.net>
2015-12-11 12:40:16 +0000

    Improved Router to work from maximum possible URL instead of from minimum. This will mean that deeper nested files/directories will take presedence over shallower. I.e: Url: /home/test/potato will now match /controllers/home/test/potato.php first instead of /controllers/home.php

--- lib/HodgePodge/Core/Router.php
+++ lib/HodgePodge/Core/Router.php
@@ -10,7 +10,7 @@ class Router
                                                     // - e.g. https://www.example.com/[example/url]?query_string -> 'example/url'
     protected static $filename;                     // Local path to php file as calculated from URL
     protected static $parts = array();              // Array of secondary arguments passed as directories within the url
-    protected static $routed_parts = array();       // Parts of the path used to calculate local path
+    protected static $unrouted_parts = array();       // Parts of the path not used to calculate local path
     public static $default = 'home';                // Path to default to if no path detected
     public static $extension = '.php';                // Path to default to if no path detected
     public static $route_prefix = ['controllers'];  // List of controller locations (from /$root)
@@ -30,37 +30,24 @@ class Router
         static::$parts = array_filter(explode('/', static::$path));
         if (!static::$parts[0]) static::$parts[0] = static::$default; // If first part is empty, replace with default
         
-        // Keep pulling file path toeksn from static::$parts until we find a routable target, or run out of parts
-        do {
-            $continue_path_traversal = false;
-            
-            // Push next tokenised part of path onto include path
-            // (The remaining $parts are used as the controller arguments)
-            static::$routed_parts[] = array_shift(static::$parts);
+        // Keep popping file path tokens from static::$parts until we find a routable target, or run out of parts
+        while (count(static::$parts)) {
             
             // Build filename
             foreach(static::$route_prefix as $pre) {
                 $path = static::$root
                     . $pre . DIRECTORY_SEPARATOR
-                    . implode(DIRECTORY_SEPARATOR, static::$routed_parts);
+                    . implode(DIRECTORY_SEPARATOR, static::$parts);
                     
                 // If file exists, then include controller and return.
                 if(file_exists($path . static::$extension)) {
                     return static::$filename = $path . static::$extension;
                 }
-                
-                if(is_dir($path)) {
-                    $continue_path_traversal = true;
-                }
             }
             
-            // Could not find a file to include
-            if (!$continue_path_traversal) {
-                // We did not detected a deeper matching directory we could traverse into. Bail out!
-                throw new HTTP\NotFound();
-            }
-            
-        } while (count(static::$parts));
+            // Pop the next token off the path, and prepend it to the list of unrouteable parts
+            array_unshift(static::$unrouted_parts, array_pop(static::$parts));
+        }
         
         // Could not find a page to route to - throw exception
         throw new HTTP\NotFound();
@@ -68,7 +55,7 @@ class Router
     
     public static function args()
     {
-        return static::$parts;
+        return static::$unrouted_parts;
     }
 
     public static function path()
