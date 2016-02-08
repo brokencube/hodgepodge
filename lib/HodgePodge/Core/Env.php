cc0febd16dc06594815c702bdcf40b77acd8b758
Nik Barham <nick.barham@musicgateway.net>
2016-02-08 11:56:09 +0000

    Added a couple more bot names

--- lib/HodgePodge/Core/Env.php
+++ lib/HodgePodge/Core/Env.php
@@ -113,7 +113,7 @@ class Env
         );
         
         $this->bot = preg_match(
-            '/(Exabot|AhrefsBot|Googlebot|MegaIndex|YandexBot|Yahoo\! Slurp|Domain Re-Animator Bot|Baiduspider|bingbot|QuerySeekerSpider)/i',
+            '/(MJ12bot|trendictionbot|Exabot|AhrefsBot|Googlebot|MegaIndex|YandexBot|Yahoo\! Slurp|Domain Re-Animator Bot|Baiduspider|bingbot|QuerySeekerSpider)/i',
             $_SERVER['HTTP_USER_AGENT']
         );
         
